b12f8c0be4f8d847256c3751c3b03e99fde3611f
Yihong Yu <116992300+HazelYuAhiru@users.noreply.github.com>
2025-07-06 15:08:08 -0700

    Fix match_list (#59)
    * fix match issue
    
    * add new test case
    
    * update match_list
    
    * clen aup
    
    * update test cases naming

--- tests/test_query_rewriter.py
+++ tests/test_query_rewriter.py
@@ -1164,6 +1164,40 @@ def test_partial_matching4():
     assert format(parse(q1)) == format(parse(_q1))
 
 
+def test_rewrite_and_on_true():
+    q0 = '''
+        SELECT people.name
+        FROM people
+        WHERE 1 AND 1
+        '''
+    q1 = '''
+        SELECT people.name
+        FROM people
+        '''
+    rule_keys = ['and_on_true']
+    rules = [get_rule(k) for k in rule_keys]
+    _q1, _rewrite_path = QueryRewriter.rewrite(q0, rules)
+    print(_q1)
+    assert format(parse(q1)) == format(parse(_q1))
+
+
+def test_rewrite_multiple_and_on_true():
+    q0 = '''
+        SELECT name
+        FROM people
+        WHERE 1 = 1 AND 2 = 2
+        '''
+    q1 = '''
+        SELECT name
+        FROM people
+        '''
+    rule_keys = ['multiple_and_on_true']
+    rules = [get_rule(k) for k in rule_keys]
+    _q1, _rewrite_path = QueryRewriter.rewrite(q0, rules)
+    print(_q1)
+    assert format(parse(q1)) == format(parse(_q1))
+
+
 def test_rewrite_rule_remove_where_true():
     q0 = '''
         SELECT *
